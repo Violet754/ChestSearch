b05ba812701b1d3ceeb2b6216cd19c8b3131faef
coldrock <william.e.constable@gmail.com>
2014-07-01 21:42:26 -0700

    basic structure

---
+++ src/main/java/com/chestsearch/core/client/ClientProxy.java
@@ -0,0 +1,13 @@
+package com.chestsearch.core.client;
+
+import com.chestsearch.core.CommonProxy;
+
+
+public class ClientProxy extends CommonProxy {
+        
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+        
+}
