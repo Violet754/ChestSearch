8e87357b92aceef9db8e9937bd5338d5ca0386c3
coldrock <william.e.constable@gmail.com>
2014-07-03 02:36:08 -0700

    Added: chests light up when you look at them [temporary testing feature]

--- src/main/java/com/chestsearch/core/event/Tick.java
+++ src/main/java/com/chestsearch/core/event/Tick.java
@@ -1,21 +1,36 @@
 package com.chestsearch.core.event;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MovingObjectPosition;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.Phase;
 import cpw.mods.fml.common.gameevent.TickEvent.PlayerTickEvent;
-import cpw.mods.fml.common.gameevent.TickEvent.RenderTickEvent;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+
+
+
+
+
+
+
 @SideOnly(Side.CLIENT)
 public class Tick {
 	private Minecraft mc; 
+	private EntityLivingBase living;
 	@SubscribeEvent
 	public void onPlayerTick(PlayerTickEvent event){
-		
+		MovingObjectPosition Pos = event.player.rayTrace(100, 1.0F);
+		if( Pos.equals(living) == false){
+			
+			 Block block = mc.getMinecraft().theWorld.getBlock(Pos.blockX, Pos.blockY, Pos.blockZ);
+			 if(block.getUnlocalizedName().contains("chest") == true){
+				 block.setLightLevel(17);
+			 }
+			
+		}
   }
+	
 }
 
